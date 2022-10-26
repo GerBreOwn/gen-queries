ce44e5713835f2be5601b90f3320c782f154e8f8
Gerald S. Brown <gbrown@gerbreown.com>
2022-10-26 21:28:10 +0800

    Generated this file on 2022-10-26 21:28:10

--- gen-queries.py
+++ gen-queries.py
@@ -3,31 +3,6 @@
 import psycopg
 import example_psql as creds
 import itertools
+import get-names
 
-tbls = []
 
-conn_string = "host=" +  creds.PGHOST + " port=" + "5432" +  " dbname=" + creds.PGDATABASE +  " user=" + creds.PGUSER +  " password=" +  creds.PGPASSWORD
-conn = psycopg.connect(conn_string)
-cursor = conn.cursor()
-cursor.execute("select relname from pg_class where relkind='r' and relname !~ '^(pg_|sql_)';")
-tbls = cursor.fetchall()
-t1 = list(itertools.chain(*tbls))
-print("Tables",t1)
-
-column_names = []
-x = len(t1)
-with conn.cursor() as cursor:
-	for i in range (x):
-		# ~ print("T1", t1[i])
-		y = t1[i]
-		# ~ print("Y", y)
-		ce = f"select column_name from information_schema.columns where table_schema = 'public' and table_name= '{y}'"
-		# ~ print("CE",ce)
-		cursor.execute(ce)
-		
-		# ~ column_names = cursor.fetchall()
-		column_names.append([row[0] for row in cursor])
-		print("R0:", row[0])
-# ~ cn = list(itertools.chain(*column_names))
-print("Cols", column_names)
-# ~ print("Column names: {}\n".format(column_names))
